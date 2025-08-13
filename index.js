5d5e3a6ababee59ad1bd3c4fec5004b77feee3a4
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2025-08-13 17:25:58 -0600

    SAve index

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ $(document).off('keydown').on('keydown', function (ex) { ex.preventDefault(); })
 $(document).off('click').on('click', function (ex) { ex.preventDefault(); });
 
 /*--CONTROL READY DOCUMENT--*/
-$(document).off('ready').on('ready', function () {
+function setCSS(){
     $('body').css({ backgroundColor: 'rgba(0,0,0,1)', margin: 0, padding: 0, userSelect: 'none', pointerEvents: 'none' });
     ito = location.href.split('#')[1] || 'menu';
     getKNL();
     return false;
-});
+}
 
 function getKNL(){
     $('body').empty();
