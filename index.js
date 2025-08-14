96bf1b25486c7fcbdbe1b8ced779a0ebfa0677e7
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2025-08-13 18:21:03 -0600

    save js

--- index.js
+++ index.js
@@ -5,30 +5,38 @@ $(document).off('keydown').on('keydown', function (ex) { ex.preventDefault(); })
 $(document).off('click').on('click', function (ex) { ex.preventDefault(); });
 
 /*--CONTROL READY DOCUMENT--*/
-function setCSS(){
+function setCSS() {
     $('body').css({ backgroundColor: 'rgba(0,0,0,1)', margin: 0, padding: 0, userSelect: 'none', pointerEvents: 'none' });
     ito = location.href.split('#')[1] || 'menu';
     getKNL();
     return false;
 }
 
-function getKNL(){
+/*--GET LIST KNLS FROM GIHUB--*/
+function getKNL() {
+    fetch('https://raw.githubusercontent.com/deswes2021/tvw/main/lista.js')
+        .then(rs0 => rs0.text())
+        .then(dt0 => { setKNL(dt0); });
+    return false;
+}
+
+/*--SET LIST KNLS TO XPLAYER--*/
+function setKNL(dats) {
     $('body').empty();
     $('<div id="xbody"></div>').css({
-        position:'absolute', backgroundColor:'rgba(64,64,64,0.5)', border:'1px solid silver', inset:'1px', display:'flex',
-        flexWrap:'wrap', justifyContent:'center', alignContent:'center'
+        position: 'absolute', backgroundColor: 'rgba(64,64,64,0.5)', border: '1px solid silver', inset: '1px', display: 'flex',
+        flexWrap: 'wrap', justifyContent: 'center', alignContent: 'center'
     }).appendTo('body');
     $('<div id="xplayer"></div>').css({
-        maxWidth:'99%', maxHeight:'99%', display:'flex', flexWrap:'wrap', justifyContent:'center', overflowX:'hidden',
-        overflowY:'scroll', scrollbarWidth:'none' 
+        maxWidth: '99%', maxHeight: '99%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', overflowX: 'hidden',
+        overflowY: 'scroll', scrollbarWidth: 'none'
     }).appendTo('#xbody');
-    /*--GET-LIST-------------------------------------------------------------------------*/
-    fetch('https://raw.githubusercontent.com/deswes2021/tvw/main/lista.js')
-    .then(r0 => r0.text())
-    .then(d0 => {
-        d0.forEach(el => {
-            $('<input type="image" src="'+el.logo+'">').css({ width:'300px', height:'160px' });
-        });
+    /*--SET-LIST-------------------------------------------------------------------------*/
+    var data = $.trim(dats);
+    if (!/^(\[)/i.test(data)) { return; }
+    data.forEach(el => {
+        $('<input type="image" src="' + el.logo + '">')
+            .css({ width: '300px', height: '160px' }).appendTo('#xplayer');
     });
     return false;
 }
